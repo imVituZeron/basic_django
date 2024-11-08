4431317e51633eaea518406eec9e6fec6df28d71
Vitor De Paula Santos <vitorpaulasantos@gmail.com>
2024-11-08 15:08:15 -0300

    adding some variable into templates

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 
 def index(req):
+    context:dict = {
+        'text': "hi blog",
+        'title': "Blog"
+    }
     # it fetch into the app's template folder 
     return render(
         req,
-        'blog/index.html'
+        'blog/index.html',
+        context
     )
 
 def example(req):
+    context:dict = {
+        'text': "hi example",
+        'title': "Example"
+    }
+
     return render(
         req,
-        'blog/example.html'
+        'blog/example.html',
+        context
     )
 
--- home/views.py
+++ home/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 def index(req) :
     print('home')
 
+    context:dict = {
+        'text': "haha agora sim posso ver algo!",
+        'title': "Home"
+    }
+
     # it fetch into the app's template folder 
     return render(
         req,
-        'home/index.html'
+        'home/index.html',
+        context
     )
